df4b71cf8413eb509a29eca09375482c6196920b
Massimo Maino <maintux@gmail.com>
2013-03-15 10:18:32 +0100

    get just csv script

--- lib/client_side_validations/rails_2/action_view/form_helper.rb
+++ lib/client_side_validations/rails_2/action_view/form_helper.rb
@@ -2,7 +2,7 @@ module ClientSideValidations::Rails2::ActionView::Helpers
   module FormHelper
     def form_for(record_or_name_or_array, *args, &proc)
       html = super
-      if match = html.match(/<script.+$/)
+      if match = html.match(/<script data-csv.+$/)
         concat(match[0])
       end
     end
