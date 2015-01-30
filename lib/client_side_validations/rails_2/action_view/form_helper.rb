ce02aab25cf0edd94fe1aeb8dcb5771910528ea6
Massimo Maino <maintux@gmail.com>
2015-01-30 15:36:09 +0100

    fix

--- lib/client_side_validations/rails_2/action_view/form_helper.rb
+++ lib/client_side_validations/rails_2/action_view/form_helper.rb
@@ -2,7 +2,7 @@ module ClientSideValidations::Rails2::ActionView::Helpers
   module FormHelper
     def form_for(record_or_name_or_array, *args, &proc)
       html = super
-      if match = html.match(/<script data-csv.+$/)
+      if html.match(/<script data-csv.+<\/script>$/)
         splitted = html.split('<script data-csv')
         concat('<script data-csv'+splitted.last)
       end
